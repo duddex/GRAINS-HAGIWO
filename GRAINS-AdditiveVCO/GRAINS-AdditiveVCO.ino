60664931360dd65af52d0e40c030527388e775a2
Stefan <stefan.dudde@gmail.com>
2021-12-30 16:45:49 +0100

    Adapted Additive VCO to work with GRAINS

--- GRAINS-AdditiveVCO/GRAINS-AdditiveVCO.ino
+++ GRAINS-AdditiveVCO/GRAINS-AdditiveVCO.ino
@@ -49,18 +49,18 @@ void setup()
 }
 void updateControl() {
  //harmonics
- harm_knob = (mozziAnalogRead(3) / 4) + (mozziAnalogRead(5) / 4);
+ harm_knob = (mozziAnalogRead(2) / 4); // IN1/POT1
  harm_knob = constrain(harm_knob, 0, 255);
 
  //harmonics_gain
- gain = (mozziAnalogRead(1) / 4) + (mozziAnalogRead(4) / 4);
+ gain = (mozziAnalogRead(1) / 4); // IN2/POT2
  gain = constrain(gain, 0, 255);
 
  //OSC frequency knob
- freq1 = mozziAnalogRead(0) / 8 ;
+ freq1 = mozziAnalogRead(0) / 8 ; // POT3
 
  //frequency setting
- voct = mozziAnalogRead(7) ;
+ voct = mozziAnalogRead(3) ; // IN3
  freqv1 = freq1 * pow(2, (pgm_read_float(&(voctpow[voct])))); // V/oct apply
 
  aSin1.setFreq(freqv1); // set the frequency
